0329787511cef3fd594468e3fa0445d705f60d16
eiechuang <119470862+eiechuang@users.noreply.github.com>
2026-04-02 13:13:21 -0700

    added some normalizing sequence functions for the CSV.

--- src/ML/comparisonModel.py
+++ src/ML/comparisonModel.py
@@ -2,5 +2,5 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv("/Users/erichuang/Documents/dev/CMPE 195 Project/group-project-random-strangers/src/ML/aerobics_skeleton_dataset.csv" \
-"")
+"") #my filepath, use yours; will need to store this somewhere when actually running the model -EH
 

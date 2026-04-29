d5d8de8a86c67d2ac52fc7faf978904146d82e52
Brendan <brendanparvin7@gmail.com>
2026-04-29 02:28:50 -0700

    src/python_kinect_interface/cpp_wrapper/kinect_wrapper.cpp

--- src/python_kinect_interface/cpp_wrapper/kinect_wrapper.cpp
+++ src/python_kinect_interface/cpp_wrapper/kinect_wrapper.cpp
@@ -1,7 +1,10 @@
 #include "kinect_wrapper.h"
 #include "kinect.hpp"
+#include "rot_pos_normalizer.hpp"
 
 #include <new>
+
+const quaternionC TARGET_BASE_QUATERNION {0, 1, 0, 0};
 
 extern "C"{
 
@@ -64,21 +67,48 @@ DLLCALL long kinect_get_latest_bodies(void* device, BodyDataC* bodies, int max_b
         bodies[count].body_id = body.body_id;
         bodies[count].timestamp = timestamp;
 
-        for (size_t i = 0; i < KINECT_JOINT_COUNT; ++i) {
+        //special case for base joint
+
+        //get the normalization values
+        const coordinate_3dC required_translation = {-body.joints[0].Position.X, -body.joints[0].Position.Y, -body.joints[0].Position.Z};
+        const quaternionC required_rotation = get_rotator(quaternionC{
+                .x = body.orientations[0].Orientation.x,
+                .y = body.orientations[0].Orientation.y,
+                .z = body.orientations[0].Orientation.z,
+                .w = body.orientations[0].Orientation.w
+            }, TARGET_BASE_QUATERNION);
+
+
+        //null the base
+        bodies[count].joints[0].position = {0, 0, 0};
+        bodies[count].joints[0].orientation = TARGET_BASE_QUATERNION;
+
+        for (size_t i = 1; i < KINECT_JOINT_COUNT; ++i) {
             const auto& joint = body.joints[i];            
 
             bodies[count].joints[i].joint_type = joint.JointType;
             bodies[count].joints[i].tracking_state = joint.TrackingState;
+            
+            if(joint.TrackingState != TrackingState::TrackingState_Tracked){
+                bodies[count].joints[i].position = {0, 0, 0};
+                bodies[count].joints[i].orientation = {0, 0, 0, 0};
+            } else {
 
-            bodies[count].joints[i].position.x = joint.Position.X;
-            bodies[count].joints[i].position.y = joint.Position.Y;
-            bodies[count].joints[i].position.z = joint.Position.Z;
+                bodies[count].joints[i].position.x = joint.Position.X;
+                bodies[count].joints[i].position.y = joint.Position.Y;
+                bodies[count].joints[i].position.z = joint.Position.Z;
 
-            const auto& orientation = body.orientations[i];
-            bodies[count].joints[i].orientation.x = orientation.Orientation.x;
-            bodies[count].joints[i].orientation.y = orientation.Orientation.y;
-            bodies[count].joints[i].orientation.z = orientation.Orientation.z;
-            bodies[count].joints[i].orientation.w = orientation.Orientation.z;
+                const auto& orientation = body.orientations[i];
+                bodies[count].joints[i].orientation.x = orientation.Orientation.x;
+                bodies[count].joints[i].orientation.y = orientation.Orientation.y;
+                bodies[count].joints[i].orientation.z = orientation.Orientation.z;
+                bodies[count].joints[i].orientation.w = orientation.Orientation.z;
+
+                //normalize the position and orientation
+                bodies[count].joints[i].position = bodies[count].joints[i].position + required_translation;
+                bodies[count].joints[i].position = rotate_point(bodies[count].joints[i].position, required_rotation);
+                bodies[count].joints[i].orientation = rotate_quaternion_global(bodies[count].joints[i].orientation, required_rotation);
+            }
         }
 
         ++count;
